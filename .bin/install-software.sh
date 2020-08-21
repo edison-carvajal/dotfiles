1c656d664122d49dfd71dfac15b22d80535dc399
Edison Andres Carvajal Betancur <edisonacarvajal@gmail.com>
2020-08-21 10:21:15 -0500

    Update install-software.sh

--- .bin/install-software.sh
+++ .bin/install-software.sh
@@ -1,6 +1,6 @@
 cp sources.list /etc/apt/sources.list
-apt-get upgrade
-apt-get dselect
+sudo apt-get upgrade
+sudo apt-get dselect
 dselect update
 dpkg --set-selections < installed-software.log
 dselect install
